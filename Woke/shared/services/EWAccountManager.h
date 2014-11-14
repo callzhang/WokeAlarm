61c9772b1af5bc34cf00a16929e7d0ed74d1f68c
Zitao Xiong <caoer115@gmail.com>
2014-11-13 23:19:35 -0500

    account manager

---
+++ Woke/shared/services/EWAccountManager.h
@@ -0,0 +1,13 @@
+//
+//  EWAccountManager.h
+//  Woke
+//
+//  Created by Zitao Xiong on 13/11/2014.
+//  Copyright (c) 2014 wokealarm. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EWAccountManager : NSObject
+
+@end
