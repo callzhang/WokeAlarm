65dd7629b8391225f9f75fb201ea3b7b6fb5e669
Zitao Xiong <caoer115@gmail.com>
2014-11-23 16:55:09 -0500

    fix test

---
+++ Woke/iphone/viewmodels/EWSleepViewModel.m
@@ -0,0 +1,13 @@
+//
+//  EWSleepViewModel.m
+//  Woke
+//
+//  Created by Zitao Xiong on 23/11/2014.
+//  Copyright (c) 2014 wokealarm. All rights reserved.
+//
+
+#import "EWSleepViewModel.h"
+
+@implementation EWSleepViewModel
+
+@end
