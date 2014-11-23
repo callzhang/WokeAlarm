704659c6eac733feda0ff4c709d2649133fb2253
Zitao Xiong <caoer115@gmail.com>
2014-11-22 21:05:02 -0500

    timer

---
+++ Woke/iphone/controllers/EWSleepViewController.h
@@ -0,0 +1,13 @@
+//
+//  EWSleepViewController.h
+//  Woke
+//
+//  Created by Zitao Xiong on 22/11/2014.
+//  Copyright (c) 2014 wokealarm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EWSleepViewController : UIViewController
+
+@end
---
+++ Woke/iphone/controllers/EWSleepViewController.m
@@ -0,0 +1,37 @@
+//
+//  EWSleepViewController.m
+//  Woke
+//
+//  Created by Zitao Xiong on 22/11/2014.
+//  Copyright (c) 2014 wokealarm. All rights reserved.
+//
+
+#import "EWSleepViewController.h"
+
+@interface EWSleepViewController ()
+
+@end
+
+@implementation EWSleepViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
