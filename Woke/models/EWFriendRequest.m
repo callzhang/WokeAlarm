956e0eea1c9f90fb46535d2cae36704134e6c361
Lei Zhang <leizhang0121@gmail.com>
2015-02-09 23:32:46 -0500

    fixed EWFriendRequest validate

--- Woke/models/EWFriendRequest.m
+++ Woke/models/EWFriendRequest.m
@@ -12,5 +12,30 @@ NSString * const EWFriendshipRequestFriended = @"friend_request_friended";
 @end
 
 @implementation EWFriendRequest
+- (BOOL)validate{
+    BOOL good = YES;
+    if (!self.sender) {
+        DDLogError(@"Missing sender %@", self.serverID);
+    }
+    
+    if (!self.receiver) {
+        DDLogError(@"Missing receiver %@", self.serverID);
+    }
+    
+    if (!self.status) {
+        DDLogError(@"Missing status %@", self.serverID);
+    }
+    
+    return good;
+}
 
+- (EWPerson *)owner{
+    if (self.sender == [EWPerson me]) {
+        return self.sender;
+    }else if (self.receiver == [EWPerson me]){
+        return self.receiver;
+    }
+    
+    return nil;
+}
 @end
