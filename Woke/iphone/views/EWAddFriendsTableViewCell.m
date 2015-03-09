a075bbd9e8612d485f00fda13cecb196c266c075
Zitao Xiong <caoer115@gmail.com>
2015-03-08 23:32:37 -0400

    friend request

--- Woke/iphone/views/EWAddFriendsTableViewCell.m
+++ Woke/iphone/views/EWAddFriendsTableViewCell.m
@@ -7,6 +7,7 @@
 //
 
 #import "EWAddFriendsTableViewCell.h"
+#import "EWPersonManager.h"
 
 @implementation EWAddFriendsTableViewCell
 
@@ -18,13 +19,46 @@
 - (void)setPerson:(EWPerson *)person {
     if (_person != person) {
         _person = person;
-        self.imageView.image = person.profilePic;
+        self.profileImageView.image = person.profilePic;
         self.nameLabel.text = person.name;
-        [self.imageView applyHexagonSoftMask];
+        [self.profileImageView applyHexagonSoftMask];
+        
+        @weakify(self);
+        [RACObserve(self.person, friendshipStatus) subscribeNext:^(NSNumber *status) {
+            @strongify(self);
+            DDLogVerbose(@"RAC friendship status: %@", status);
+            EWFriendshipStatus statusValue = [status integerValue];
+            switch (statusValue) {
+                case EWFriendshipStatusNone:
+                case EWFriendshipStatusUnknown:
+                    [self.rightButton setImage:[ImagesCatalog wokeAddFriendsAddFriendButtonSmall] forState:UIControlStateNormal];
+                    [self.rightButton setImage:[ImagesCatalog wokeAddFriendsAddFriendButtonSmallHighlighted] forState:UIControlStateHighlighted];
+                    break;
+                case EWFriendshipStatusSent:
+                    [self.rightButton setImage:[ImagesCatalog wokeAddFriendsFriendRequestSentButtonSmall] forState:UIControlStateNormal];
+                    [self.rightButton setImage:[ImagesCatalog wokeAddFriendsFriendRequestSentButtonSmallHighlighted] forState:UIControlStateHighlighted];
+                    break;
+                case EWFriendshipStatusDenied:
+                case EWFriendshipStatusFriended:
+                case EWFriendshipStatusReceived:
+                    //TODO: missing states
+                    [self.rightButton setImage:[ImagesCatalog wokeAddFriendsFriendRequestSentButtonSmall] forState:UIControlStateNormal];
+                    [self.rightButton setImage:[ImagesCatalog wokeAddFriendsFriendRequestSentButtonSmallHighlighted] forState:UIControlStateHighlighted];
+                    break;
+                default:
+                    break;
+            }
+        }];
     }
 }
 
 - (IBAction)onAddFriendButton:(id)sender {
-    
+    [[EWPersonManager shared] requestFriend:self.person
+                                 completion:^(EWFriendshipStatus status, NSError *error) {
+                                     DDLogVerbose(@"friend request sent, status changed to :%@", @(status));
+                                     if (error) {
+                                         DDLogError(@"got friend request sending error:%@", error);
+                                     }
+                                 }];
 }
 @end
