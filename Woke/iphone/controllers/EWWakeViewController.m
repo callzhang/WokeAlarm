45eb639a2898c266b84ffd8cfaf87973dd7bef3a
Zitao Xiong <caoer115@gmail.com>
2015-03-28 15:17:52 -0400

    fix #97

--- Woke/iphone/controllers/EWWakeViewController.m
+++ Woke/iphone/controllers/EWWakeViewController.m
@@ -16,6 +16,7 @@
 #import "EWRecordingViewController.h"
 #import "FBKVOController.h"
 #import "EWUIUtil.h"
+#import "AFNetworkReachabilityManager.h"
 
 @interface EWWakeViewController ()
 @property (weak, nonatomic) IBOutlet UIImageView *profileImageView;
@@ -24,10 +25,16 @@
 @property (weak, nonatomic) IBOutlet UILabel *wantsToWakeUpAtLabel;
 @property (weak, nonatomic) IBOutlet UILabel *statusLabel;
 @property (weak, nonatomic) IBOutlet UIButton *wakeButton;
+@property (weak, nonatomic) IBOutlet UIButton *nextButton;
+@property (nonatomic, strong) RACDisposable *reachabilityDisposable;
 
 @end
 
 @implementation EWWakeViewController
+
+- (void)dealloc {
+    [self.reachabilityDisposable dispose];
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -48,6 +55,17 @@
     if ([EWPersonManager shared].wakeeList.count == 0 && !_nextWakee) {
         [EWUIUtil showWatingHUB];
     }
+    
+    @weakify(self);
+    self.reachabilityDisposable = [RACObserve([AFNetworkReachabilityManager sharedManager], reachable) subscribeNext:^(NSNumber *reachable) {
+        @strongify(self);
+        if (reachable.boolValue) {
+            [self updateViewWithCurrentWakee];
+        }
+        else {
+            [self updateViewForOffline];
+        }
+    }];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
@@ -69,11 +87,32 @@
 - (void)setNextWakee:(EWPerson *)nextWakee {
     _nextWakee = nextWakee;
     
+    [self updateViewWithCurrentWakee];
+}
+
+- (void)updateViewForOffline {
+    self.profileImageView.image = [ImagesCatalog wokePlaceholderUserProfileImageOther];
+    self.nameLabel.alpha = 0.5;
+    self.wantsToWakeUpAtLabel.alpha = 0.5;
+    self.statusLabel.alpha = 0.5;
+    
+    self.wakeButton.enabled = NO;
+    self.nextButton.enabled = NO;
+}
+
+- (void)updateViewWithCurrentWakee {
+    EWPerson *nextWakee = self.nextWakee;
     self.profileImageView.image = nextWakee.profilePic;
+    self.nameLabel.alpha = 1.0f;
     self.nameLabel.text = nextWakee.name;
     NSDate *nextTime = [[EWAlarmManager sharedInstance] nextAlarmTimeForPerson:nextWakee];
     self.wantsToWakeUpAtLabel.text = [NSString stringWithFormat:@"wants to wake up at %@", [nextTime mt_stringFromDateWithHourAndMinuteFormat:MTDateHourFormat12Hour]];
+    self.wantsToWakeUpAtLabel.alpha = 1.0f;
     self.statusLabel.text = [[EWAlarmManager sharedInstance] nextStatementForPerson:nextWakee];
+    self.statusLabel.alpha = 1.0f;
+
+    self.wakeButton.enabled = YES;
+    self.nextButton.enabled = YES;
 }
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender{
