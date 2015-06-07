6ec676f1f147a5e49554d2fe57ee9dbf8a3b6bce
Zitao Xiong <caoer115@gmail.com>
2015-06-07 17:38:05 -0400

    fix #136

--- Woke/shared/controllers/EWStyleController.m
+++ Woke/shared/controllers/EWStyleController.m
@@ -14,5 +14,7 @@
     NSDictionary *navbarTitleTextAttributes = @{NSForegroundColorAttributeName: [UIColor whiteColor], NSFontAttributeName:[UIFont fontWithName:@"Lato-Regular" size:20.0]};
     [[UINavigationBar appearance] setTitleTextAttributes:navbarTitleTextAttributes];
     
+    [[UIView appearanceWhenContainedIn:[UIAlertController class], nil] setTintColor:[UIColor colorWithRed:0 green:122/255.0 blue:1.0 alpha:1.0]];
+
 }
 @end
