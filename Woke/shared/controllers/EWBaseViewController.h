c1d0b01a6d19a5762cdd296159b80e11494c9773
Lei <leizhang0121@gmail.com>
2015-06-17 21:01:17 -0400

    1

---
+++ Woke/shared/controllers/EWBaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  EWBaseViewController.h
+//  Woke
+//
+//  Created by Lei on 6/17/15.
+//  Copyright (c) 2015 wokealarm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EWBaseViewController : UIViewController
+
+@end
